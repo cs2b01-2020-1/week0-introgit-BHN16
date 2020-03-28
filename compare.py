fbccfd7b9b5a92ae45b82d265fb91b9b70291dbb
bhn16 <dario.toribio@utec.edu.pe>
2020-03-27 23:24:09 -0500

    Genoma code

---
+++ compare.py
@@ -0,0 +1,34 @@
+import os
+import glob
+import numpy as np
+
+def comparacion(a, b):
+    coincidence = 0
+    size = None
+    if(len(a) == len(b)):
+        size = len(a)
+    else:
+        size = min(len(a), len(b))
+    for i in range(0, size):
+        if(a[i] == b[i]):
+            coincidence += 1
+    return round(((coincidence/size)*100),2)             
+        
+                
+#location = os.getcwd() + "/genomas"
+files = glob.glob(os.getcwd()+"/genomas/*.txt")
+read = None
+arr = None
+size = None
+for file in files:
+    read = open(file, "r")
+    arr = read.readlines()
+    size = len(arr)
+    chart = np.zeros((size,size))
+    for i in range(1, size):
+        for j in range(1, size):
+            if(j != i):
+                chart[i][j] = comparacion(arr[i], arr[j]) 
+    arr.clear()
+    read.close()
+    print(chart)
--- hello.py
+++
@@ -1,3 +0,0 @@
-nombre = input()
-print("Hola " + nombre)
-print("Welcome to CS2B01")
